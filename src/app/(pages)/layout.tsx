e5e57ba68a871b4d798a4f58a7838591c6d8aa6b
Steshenko Dmitry <3301297@gmail.com>
2025-04-23 16:21:12 +0300

    refactor: remove home page

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,14 +1,19 @@
+import { getServerSession } from "next-auth";
 import { Header } from "@/components/header/header";
 import { Menu } from "@/components/menu/menu";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface PagesLayoutProps {
   children: React.ReactNode;
 }
 
-export default function PagesLayout({ children }: PagesLayoutProps) {
+export default async function PagesLayout({ children }: PagesLayoutProps) {
+  const session = await getServerSession(authOptions);
+  const { user } = session || {};
+
   return (
     <>
-      <Header name={"name"} avatarUrl="" />
+      <Header name={user?.name || ""} avatarUrl={user?.image || ""} />
       <div className="flex min-h-screen">
         <Menu />
         <main>{children}</main>
--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,5 +1,5 @@
 import { signIn, signOut } from "next-auth/react";
 
 export const handleSignIn = async () =>
-  await signIn("google", { callbackUrl: "/home" });
+  await signIn("google", { callbackUrl: "/projects" });
 export const handleSignOut = async () => await signOut();
